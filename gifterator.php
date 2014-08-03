1c4e2f6de59e8b2dfe760ed23356bd07f844760a
Petro Rogutskyi <arhept@gmail.com>
2014-08-03 13:41:46 +0200

    menu added

--- gifterator.php
+++ gifterator.php
@@ -6,7 +6,11 @@
 add_action('admin_menu', 'add_gifterator_menu');
  
 function add_gifterator_menu() {
-	echo '<p>This is the options menu<p>';
+	add_menu_page( 'Prezenty', 'Prezenty', 'edit_pages', 'prezenty', 'build_gifterator_menu', null, 21);
+}
+
+function build_gifterator_menu() {
+	echo '<p>Awesome menu</p>';
 }
 
 ?>
